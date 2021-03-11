b16f07c0721bbd92da260271304ea614f548f86b
mrelectrq <tiora100bass@gmail.com>
2021-03-12 01:14:45 +0200

    Identity Models definition

---
+++ RVT.Monitoring.Identity/Data/IdentityModel/RVTRole.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RVT.Monitoring.Identity.Data.IdentityModel
+{
+    public class RVTRole : IdentityRole<Guid>
+    {
+         public string Description { get; set; }
+    }
+}
---
+++ RVT.Monitoring.Identity/Data/IdentityModel/RVTUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RVT.Monitoring.Identity.Data.IdentityModel
+{
+    public class RVTUser : IdentityUser<Guid>
+    {
+         public string PhoneNumber { get; set; }
+        public DateTime RegisterTimeStamp { get; set; }
+    }
+}
