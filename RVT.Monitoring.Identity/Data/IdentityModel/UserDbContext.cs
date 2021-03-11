775e59be9cca3c63ab7fd24b2c3f80745bc668fd
mrelectrq <tiora100bass@gmail.com>
2021-03-12 01:15:22 +0200

    Added dbContext for User and Roles manager

---
+++ RVT.Monitoring.Identity/Data/IdentityModel/UserDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RVT.Monitoring.Identity.Data.IdentityModel
+{
+    public class UserDbContext : IdentityDbContext<RVTUser, RVTRole, Guid>
+    {
+
+        public UserDbContext(DbContextOptions<UserDbContext> opt) : base(opt)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
