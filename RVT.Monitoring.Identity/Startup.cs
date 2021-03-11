3a3f8caafe30263adf3c72624a6fc177623f8d0b
mrelectrq <tiora100bass@gmail.com>
2021-03-12 01:16:49 +0200

    IdentityServer , default identity model, dbcontext configurations

--- RVT.Monitoring.Identity/Startup.cs
+++ RVT.Monitoring.Identity/Startup.cs
@@ -1,14 +1,21 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
+using IdentityServer4.EntityFramework.DbContexts;
+using IdentityServer4.EntityFramework.Mappers;
 using IdentityServerHost.Quickstart.UI;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using RVT.Monitoring.Identity.Data;
+using RVT.Monitoring.Identity.Data.IdentityModel;
 
 namespace RVT.Monitoring.Identity
 {
@@ -18,18 +25,44 @@ namespace RVT.Monitoring.Identity
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            var migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
+
+
+            services.AddDbContext<UserDbContext>(opt =>
+            {
+                opt.UseSqlServer(Configuration.IDENTITY_USERS_CONNECION);
+            });
+
+            services.AddIdentity<RVTUser,RVTRole>()
+               .AddEntityFrameworkStores<UserDbContext>()
+               .AddDefaultUI()
+               .AddDefaultTokenProviders()
+               .AddRoles<RVTRole>();
             services.AddMvc(opt =>
             {
                 opt.EnableEndpointRouting = false;
                 });
             services.AddIdentityServer()
                 .AddDeveloperSigningCredential()
-                .AddInMemoryApiResources(Configuration.GetApiResources())
-                .AddInMemoryClients(Configuration.GetClients())
                 .AddTestUsers(TestUsers.Users)
-                .AddInMemoryIdentityResources(Configuration.GetIdentityResources());
-                
-            
+                .AddConfigurationStore(opt =>
+                {
+                    opt.ConfigureDbContext = builder =>
+                    builder.UseSqlServer(Configuration.IDENTITYDB_CONNECION,
+                        sql => sql.MigrationsAssembly(migrationsAssembly));
+                })
+                .AddOperationalStore(opt =>
+                {
+                    opt.ConfigureDbContext = builder =>
+                   builder.UseSqlServer(Configuration.IDENTITYDB_CONNECION,
+                       sql => sql.MigrationsAssembly(migrationsAssembly));
+                    opt.EnableTokenCleanup = true;
+                    opt.TokenCleanupInterval = 30;
+
+                });
+
+            //services.AddDefaultIdentity<IdentityUser>()
+            //    .AddRoles<IdentityRole>(); Need to continue 
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,7 +73,7 @@ namespace RVT.Monitoring.Identity
                 app.UseDeveloperExceptionPage();
             }
 
-
+            InitializeDatabase(app);
             app.UseIdentityServer();
             app.UseStaticFiles();
             app.UseMvcWithDefaultRoute();
@@ -54,6 +87,43 @@ namespace RVT.Monitoring.Identity
             //        await context.Response.WriteAsync("Hello World!");
             //    });
             //});
+        }
+
+        private void InitializeDatabase(IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+            {
+                serviceScope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>().Database.Migrate();
+
+                var context = serviceScope.ServiceProvider.GetRequiredService<ConfigurationDbContext>();
+                context.Database.Migrate();
+                if (!context.Clients.Any())
+                {
+                    foreach (var client in Configuration.GetClients())
+                    {
+                        context.Clients.Add(client.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+
+                if (!context.IdentityResources.Any())
+                {
+                    foreach (var resource in Configuration.GetIdentityResources())
+                    {
+                        context.IdentityResources.Add(resource.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+
+                if (!context.ApiResources.Any())
+                {
+                    foreach (var resource in Configuration.GetApiResources())
+                    {
+                        context.ApiResources.Add(resource.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+            }
         }
     }
 }
